f975a68df51ee2ff9083a8db8f6371b7c7d9a8d3
zeeshan <zeeshanansari38244@gmail.com>
2016-07-21 10:59:28 +0500

    title is complete

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="./css/style.css" type="text/css">
+</head>
+<body>
+
+   <table id="mainTable" >
+    <!-- Title of website -->
+     <tr class"title" >
+       <td class="title" ><h1>Welcome to CMS website<h1></td>
+     </tr>
+     
+      <!-- navigation bar  of website -->
+     <tr>
+       <td></td>
+     </tr>
+     
+      <!-- content of website -->
+     <tr>
+       <td></td>
+     </tr>
+
+      <!-- Title of website -->
+     <tr>
+       <td></td>
+     </tr>
+
+      <!-- Title of website -->
+     <tr>
+       <td></td>
+     </tr>
+   
+   </table>
+
+    
+</body>
+</html>
